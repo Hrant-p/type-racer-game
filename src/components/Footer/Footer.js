2237cb1e5444c1b81842a1d8db243cd8ee29174e
Hrant-p <hrant@live.ru>
2020-02-10 02:41:44 +0400

    create Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    );
-};
+const StyledP = styled.p`
+  color: white;
+  text-shadow: 1px 1px 1px silver;
+    @media (max-width: 768px) {
+    font-size: 0.75rem;
+  }
+`;
+
+const Footer = () => (
+  <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'indigo'
+  }}
+  >
+    <StyledP>
+      {new Date().getFullYear()}
+      {' '}
+© ✨ Developed with React & Redux By Hrant Poghosyan 👌
+    </StyledP>
+  </div>
+);
 
 export default Footer;
