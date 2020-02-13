ad143fa6d530403632884198e94e7c36ea88cf98
Hrant-p <hrant@live.ru>
2020-02-13 20:15:44 +0400

    Updates

---
+++ precache-manifest.3f5f0b69d9064987b2720afe8f179389.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13d7dc334586a3ca53c04ee1dc41edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b15d4f7d70ffc64face2",
+    "url": "/static/css/3.c51a11e5.chunk.css"
+  },
+  {
+    "revision": "7b835cf6ce1a6849142f",
+    "url": "/static/css/4.653ff131.chunk.css"
+  },
+  {
+    "revision": "3340e7848aa636b92aaa",
+    "url": "/static/css/5.653ff131.chunk.css"
+  },
+  {
+    "revision": "df73f597da9608bbc7e8",
+    "url": "/static/css/6.031c8ad0.chunk.css"
+  },
+  {
+    "revision": "884bf0a02b54cb1ed27c",
+    "url": "/static/css/main.56fce5ad.chunk.css"
+  },
+  {
+    "revision": "ba6eaef39927ea34fb5d",
+    "url": "/static/js/2.7fdf62cf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7fdf62cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b15d4f7d70ffc64face2",
+    "url": "/static/js/3.c4d7f843.chunk.js"
+  },
+  {
+    "revision": "7b835cf6ce1a6849142f",
+    "url": "/static/js/4.71765dea.chunk.js"
+  },
+  {
+    "revision": "3340e7848aa636b92aaa",
+    "url": "/static/js/5.e041e6db.chunk.js"
+  },
+  {
+    "revision": "df73f597da9608bbc7e8",
+    "url": "/static/js/6.7bb12697.chunk.js"
+  },
+  {
+    "revision": "e864a44d9d2176d2d40c",
+    "url": "/static/js/7.c29c539d.chunk.js"
+  },
+  {
+    "revision": "884bf0a02b54cb1ed27c",
+    "url": "/static/js/main.8eca18df.chunk.js"
+  },
+  {
+    "revision": "81a713ff500ed21292af",
+    "url": "/static/js/runtime-main.44d7088f.js"
+  },
+  {
+    "revision": "0572329616bd5588f1b8894acd259b0e",
+    "url": "/static/media/about.05723296.jpg"
+  },
+  {
+    "revision": "c551ee8359c99bba5c970a21a53849e8",
+    "url": "/static/media/coder.c551ee83.jpeg"
+  },
+  {
+    "revision": "d32e673d4a3b460f052da65afa117ef9",
+    "url": "/static/media/coding2.d32e673d.jpg"
+  },
+  {
+    "revision": "80b3d536c40bc32462967429b27005a6",
+    "url": "/static/media/sign-up.80b3d536.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/type-racer-game/precache-manifest.94648f262820e8f9e7b0d45c727572d1.js"
+  "/precache-manifest.3f5f0b69d9064987b2720afe8f179389.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/type-racer-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
